1ddf83cb94bb12b3129ec077b743ff8014d579b5
Lancelot <wangye0206@gmail.com>
2013-07-11 16:02:17 -0400

    CoolingSpliter.py can due with blank line correctly now

--- CoolingSpliter.py
+++ CoolingSpliter.py
@@ -15,31 +15,32 @@ import sys, os, math, queue, threading
 def Process(Cole, Ovr):
     Data = []
     tot_line = len(Ovr)
-            
     for LN in range( tot_line ):
         
         OvrColumns = Ovr[LN].split('\t')
-        # hydrogen density
-        hden = float(OvrColumns[3])
-        # electron density
-        eden = float(OvrColumns[4])
-
         ColeColumns = Cole[LN].split('\t')
-        Temp = float(ColeColumns[1])
-        hhe_cooling = 0.
-        metal_cooling = 0.
-        eeff = 0.
-        other_cooling = 0.
-        for i in range( 3, 50 ):
-            if( i == 3 or i == 4 or i == 35 or i == 36 or i == 37 or i ==38 or i == 39 or i == 40 or i == 41 or i == 45 or i == 48 ):
-                hhe_cooling += (float(ColeColumns[i])/(hden * eden))
-            elif( i == 33 or i == 34 or i == 44 or i == 46 or i == 47 ):
-                other_cooling += (float(ColeColumns[i]) / (hden * eden))
-            elif( i == 43 ):
-                eeff += float(ColeColumns[i])/(hden * eden)
-            else:
-                metal_cooling += (float(ColeColumns[i])  / (hden*eden))
-        Data.append([Temp, hden, eden, hhe_cooling, metal_cooling, eeff, other_cooling])
+        if( len(OvrColumns) >= 5 and len(ColeColumns) >=51 ):
+            # hydrogen density
+            hden = float(OvrColumns[3])
+            # electron density
+            eden = float(OvrColumns[4])
+            Temp = float(ColeColumns[1])
+            hhe_cooling = 0.
+            metal_cooling = 0.
+            eeff = 0.
+            other_cooling = 0.
+            for i in range( 3, 50 ):
+                if( i == 3 or i == 4 or i == 35 or i == 36 or i == 37 or i ==38 or i == 39 or i == 40 or i == 41 or i == 45 or i == 48 ):
+                    hhe_cooling += (float(ColeColumns[i])/(hden * eden))
+                elif( i == 33 or i == 34 or i == 44 or i == 46 or i == 47 ):
+                    other_cooling += (float(ColeColumns[i]) / (hden * eden))
+                elif( i == 43 ):
+                    eeff += float(ColeColumns[i])/(hden * eden)
+                else:
+                    metal_cooling += (float(ColeColumns[i])  / (hden*eden))
+            Data.append([Temp, hden, eden, hhe_cooling, metal_cooling, eeff, other_cooling])
+        else:
+            Data.append('\n')
         
     return Data
 
@@ -58,8 +59,13 @@ class CalThread(threading.Thread):
 
 def main():
 
-    print('Reading Files')
     ModelName = sys.argv[1]
+    if( not os.path.exists('./{}.cole'.format(ModelName))):
+        sys.exit('ERROR: I can not find cole file!')
+    if( not os.path.exists('./{}.ovr'.format(ModelName))):
+        sys.exit('ERROR: I can not find ovr file!')
+
+    print('Reading Files')
     ColeFile = open('./{}.cole'.format(ModelName), 'r')
     ColeLines = ColeFile.readlines()
     ColeFile.close()
@@ -70,9 +76,11 @@ def main():
     del OvrLines[0]
 
     print('Checking Files')
-    for i in range(1, len(ColeLines) - 1 ):
+    i = 0
+    while( i < len(ColeLines) ):
         if( ColeLines[i].find('not converged') != -1):
-            del ColLines[i]
+            del ColeLines[i]
+        i = i + 1
     if( len(ColeLines) != len(OvrLines) ):
         sys.exit('ERROR: Two original files do not contian same amount of points!')
 
@@ -94,18 +102,21 @@ def main():
         j.join()
     
     print('Exporting')
-    Output = open('Splited_Cooling.dat', 'w')
+    Output = open('./{}.scd'.format(ModelName), 'w')
     Output.write('Temperature\thden\teden\tH&He\tmetal\teeff\tother\n')
     while( not Result_Queue.empty() ):
         TempList = Result_Queue.get()
         OutputList = TempList[1]
         for LineNum in range( len(OutputList) ):
-            for ColNum in range(7):
-                Output.write('{:.4e}'.format(OutputList[LineNum][ColNum]))
-                if( ColNum == 6):
-                    Output.write('\n')
-                else:
-                    Output.write('\t')
+            if( len(OutputList[LineNum]) == 1 ):
+                Output.write(OutputList[LineNum][0])
+            else:
+                for ColNum in range(7):
+                    Output.write('{:.4e}'.format(OutputList[LineNum][ColNum]))
+                    if( ColNum == 6):
+                        Output.write('\n')
+                    else:
+                        Output.write('\t')
     Output.close()
 
 if __name__ == "__main__":
